76f095b954e93abb865d12591a2efa6d684584be
GunshipPenguin <GunshipPenguin@users.noreply.github.com>
2017-01-08 02:23:53 -0500

    Add Twitter user API keys

--- js/api-keys.js
+++ js/api-keys.js
@@ -4,6 +4,8 @@ var apiKeys = {
   newsApiKey: '',
   twitter_consumer_key: '',
   twitter_consumer_secret: '',
+  twitter_access_token_key: '',
+  twitter_access_token_secret: '',
   twitter_bearer_token: ''
 }
 
