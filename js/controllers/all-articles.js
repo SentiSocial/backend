c339d19c80c08f54c8582d3c06ad1db434c7f8ee
GunshipPenguin <GunshipPenguin@users.noreply.github.com>
2017-01-18 18:54:08 -0500

    Add tests for all-articles/tweets controllers, fix some small bugs in them

--- js/controllers/all-articles.js
+++ js/controllers/all-articles.js
@@ -5,15 +5,19 @@ const allArticlesController = function (req, res) {
   if (!req.query.max_id) {
     query = {}
   } else {
-    options = {_id: {$gt: res.query.max_id}}
+    // If max_id is provided, ensure that it is valid
+    if(isNaN(parseFloat(req.query.max_id)) || !isFinite(req.query.max_id)) {
+      res.status(400).send('Invalid max_id parameter')
+    }
+    options = {_id: {$gt: req.query.max_id}}
   }
 
   Article.find(query, (err, articles) => {
     if (err) {
       res.status(500).send('Internal error while retreiving tweets')
     } else {
-      res.type('application/json')
-      res.send(articles)
+      let resData = {articles: articles}
+      res.json(resData)
     }
   })
 }
---
+++ spec/controllers/all-tweets-spec.js
@@ -0,0 +1,62 @@
+const httpMocks = require('node-mocks-http')
+const mockery = require('mockery')
+
+describe('All Tweets Controller', () => {
+
+  var controller
+  var mockTweets = [{
+    embedId: '123456789',
+    _id: '1234abcdef'
+  }]
+
+  beforeAll (function() {
+    mockery.enable({
+      warnOnReplace: false,
+      warnOnUnregistered: false
+    })
+
+    mockery.registerMock('../models/tweet', {
+      find: (query, cb) => {
+        cb(null, mockTweets)
+      }
+    })
+
+    controller = require('../../js/controllers/all-tweets')
+  })
+
+  it('Should return the all tweets', () => {
+    let req  = httpMocks.createRequest({
+      method: 'GET',
+      url: '/v1/alltrends/tweets'
+    })
+    let res = httpMocks.createResponse()
+
+    controller(req, res)
+
+    expect(res.statusCode).toEqual(200)
+    expect(res._isEndCalled()).toEqual(true)
+    expect(res._isJSON()).toEqual(true)
+
+    let data = JSON.parse(res._getData())
+
+    expect(data.tweets.length).toEqual(mockTweets.length)
+    expect(data.tweets[0].id).toEqual(mockTweets[0].id)
+    expect(data.tweets[0]._id).toEqual(mockTweets[0]._id)
+  })
+
+  it('Should respond with HTTP 400 if an invalid max_id is provided', () => {
+    let req  = httpMocks.createRequest({
+      method: 'GET',
+      url: '/v1/alltrends/tweets',
+      query: {
+        max_id: 'asdf' // Invalid
+      }
+    })
+    let res = httpMocks.createResponse()
+
+    controller(req, res)
+
+    expect(res.statusCode).toEqual(400)
+    expect(res._isEndCalled()).toEqual(true)
+  })
+})
