74c492df1671636d957a0426bdc96b84dc5c0cc4
GunshipPenguin <GunshipPenguin@users.noreply.github.com>
2017-02-04 17:46:53 -0500

    Add test for trend controller

--- spec/controllers/trend-spec.js
+++ spec/controllers/trend-spec.js
@@ -1,44 +1,137 @@
 const httpMocks = require('node-mocks-http')
-const mockery = require('mockery')
+const Trend = require('../../src/models/trend')
+const mongoose = require('mongoose')
+const mockgoose = require('mockgoose')
+const trendController = require('../../src/controllers/trend')
 
-describe('All Trends Controller', () => {
-  var controller
-  var mockTrends = [{
-    name: '#thisIsATrend',
-    history: [{sentiment: 5, timestamp: 22}]
-  }]
+function getResponse () {
+  return httpMocks.createResponse({
+    eventEmitter: require('events').EventEmitter
+  })
+}
 
-  beforeAll(function () {
-    mockery.enable({
-      warnOnReplace: false,
-      warnOnUnregistered: false
+describe('Trend Controller', () => {
+  var mockTrend = {
+    name: 'test-trend',
+    history: [
+      {sentiment: 1, timestamp: 100},
+      {sentiment: 2, timestamp: 160}
+    ]
+  }
+
+  beforeAll(function (done) {
+    // Wrap mongoose with mockgoose
+    mockgoose(mongoose).then(() => {
+      mongoose.connect('mongodb://example.com/testdb', err => {
+        if (err) throw err
+        // Clear the database
+        mockgoose.reset(() => {
+          // Insert the mock trend
+          Trend.collection.insert([mockTrend], (err, docs) => {
+            if (err) throw err
+            done()
+          })
+        })
+      })
     })
-
-    mockery.registerMock('../models/trend', {
-      find: (query, cb) => {
-        cb(null, mockTrends)
-      }
-    })
-
-    controller = require('../../src/controllers/all-trends')
   })
 
-  it('Should return the all trends', () => {
+  afterAll((done) => {
+    mongoose.unmock(() => {
+      done()
+    })
+  })
+
+  it('Should return with status 200 for a valid request', done => {
     let req = httpMocks.createRequest({
       method: 'GET',
-      url: '/v1/alltrends'
+      params: {
+        name: 'test-trend'
+      },
+      url: '/v1/trend/test-trend'
     })
-    let res = httpMocks.createResponse()
+    let res = getResponse()
 
-    controller(req, res)
+    res.on('end', () => {
+      expect(res.statusCode).toEqual(200)
+      done()
+    })
 
-    expect(res.statusCode).toEqual(200)
-    expect(res._isEndCalled()).toEqual(true)
-    expect(res._isJSON()).toEqual(true)
+    trendController(req, res)
+  })
 
-    let data = JSON.parse(res._getData())
-    expect(data.trends.length).toEqual(mockTrends.length)
-    expect(data.trends[0].name).toEqual(mockTrends[0].name)
-    expect(data.trends[0].history).toEqual(mockTrends[0].history)
+  it('Should return with status 400 if no trend is specified', done => {
+    let req = httpMocks.createRequest({
+      method: 'GET',
+      url: '/v1/trend/test-trend'
+    })
+    let res = getResponse()
+
+    res.on('end', () => {
+      expect(res.statusCode).toEqual(400)
+      done()
+    })
+
+    trendController(req, res)
+  })
+
+  it('Should return with status 404 if a nonexistant trend is specified', done => {
+    let req = httpMocks.createRequest({
+      method: 'GET',
+      params: {
+        name: 'trend-that-does-not-exist'
+      },
+      url: '/v1/trend/test-trend'
+    })
+    let res = getResponse()
+
+    res.on('end', () => {
+      expect(res.statusCode).toEqual(404)
+      done()
+    })
+
+    trendController(req, res)
+  })
+
+  it('Should return valid JSON', done => {
+    let req = httpMocks.createRequest({
+      method: 'GET',
+      params: {
+        name: 'test-trend'
+      },
+      url: '/v1/trend/test-trend'
+    })
+    let res = getResponse()
+
+    res.on('end', () => {
+      expect(res._isJSON()).toEqual(true)
+      done()
+    })
+
+    trendController(req, res)
+  })
+
+  it('Should return all data for the trend', done => {
+    let req = httpMocks.createRequest({
+      method: 'GET',
+      params: {
+        name: 'test-trend'
+      },
+      url: '/v1/trend/test-trend'
+    })
+    let res = getResponse()
+
+    res.on('end', () => {
+      let data = JSON.parse(res._getData())
+
+      let fields = ['name', 'history']
+      fields.forEach(field => {
+        expect(data[field]).toBeDefined()
+      })
+
+      done()
+    })
+
+    trendController(req, res)
   })
 })
