308422b9d155333da65d7758a1f479fa5dbe876d
GunshipPenguin <GunshipPenguin@users.noreply.github.com>
2017-05-22 16:28:40 -0700

    Disregard 'retweet' and 'rt'

--- src/twitter/keyword-bank.js
+++ src/twitter/keyword-bank.js
@@ -4,7 +4,7 @@ const keywordExtractor = require('keyword-extractor')
 // Disregard keywords if they match these regexes
 const stopRegexes = [
   /http.*/, // Disregard links
-  /^rt$/, // Disregard 'rt' (prepended to retweets)
+  /^rt|retweet$/i, // Disregard 'rt' (prepended to retweets)
   /^[^a-zA-z0-9#@]/ // Disregard words with any non alphanumeric characters (excluding @ and #)
 ]
 
