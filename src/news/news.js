c69eeab67cce6db74829066b7a3edaa4270e9945
GunshipPenguin <me@rhysre.net>
2018-01-10 23:49:05 -0500

    Switch to News API v2

--- src/news/news.js
+++ src/news/news.js
@@ -1,11 +1,9 @@
 'use strict'
 const request = require('request')
-const newsUtils = require('../utils/news-utils')
 
-const newsApiBaseUrl = 'https://newsapi.org/v1'
+const newsApiBaseUrl = 'https://newsapi.org/v2'
 const apiKey = require('../api-keys').newsApiKey
 const config = require('../../config.js')
-var sources = require('./sources.json')
 
 const news = {
   /**
@@ -15,27 +13,9 @@ const news = {
    */
   getNews: function (phrase) {
     return new Promise((resolve, reject) => {
-      let articles = []
-      let pending = sources.length
-
-      const pattern = newsUtils.generateFuzzyPattern(phrase)
-
-      sources.forEach(source => searchForArticlesFromSource(pattern, source)
-        .then(response => {
-          pending--
-
-          articles = articles.concat(response)
-          if (pending === 0) {
-            resolve(articles.slice(0, config.maxArticlesPerTrend))
-          }
-        }).catch(error => {
-          console.error(error)
-          pending--
-          if (pending === 0) {
-            resolve(articles.slice(0, config.maxArticlesPerTrend))
-          }
-        })
-      )
+      getNewsArticlesForPhrase(phrase)
+        .then(articles => resolve(articles))
+        .catch(error => reject(error))
     })
   }
 }
@@ -47,42 +27,39 @@ const news = {
  * @param  {RegExp} pattern describing what to find
  * @param  {String} source newsapi.org source id
  */
-function searchForArticlesFromSource (pattern, source) {
+function getNewsArticlesForPhrase (phrase) {
   return new Promise((resolve, reject) => {
-    const url = `${newsApiBaseUrl}/articles?source=${source.id}&apiKey=${apiKey}`
-    const articles = []
+    const options = {
+      url: newsApiBaseUrl + '/everything?q=' + encodeURIComponent(phrase) + `&pageSize=${config.maxArticlesPerTrend}`,
+      headers: {
+        'Authorization': apiKey
+      }
+    }
 
-    request(url, (error, response) => {
+    request(options, (error, response) => {
       if (error) {
-        reject(error)
+        reject(new Error(error))
         return
       }
 
       const responseJson = JSON.parse(response.body)
-
       if (responseJson.status !== 'ok') {
-        reject(new Error(responseJson.status))
+        reject(new Error(responseJson.message))
         return
       }
 
+      const articles = []
       responseJson.articles.forEach(article => {
-        const title = article.title
-        const titleMatches = title && pattern.test(title)
-        const description = article.description
-        const descriptionMatches = description && pattern.test(description)
-
-        if (titleMatches || descriptionMatches) {
-          const timestamp = new Date(article.publishedAt).getTime() / 1000
-          articles.push({
-            title: title,
-            description: description,
-            timestamp: timestamp,
-            source: source.name,
-            link: article.url,
-            media: article.urlToImage
-          })
-        }
+        articles.push({
+          title: article.title,
+          description: article.description,
+          timestamp: new Date(article.publishedAt).getTime() / 1000,
+          source: article.source.name,
+          link: article.url,
+          media: article.urlToImage
+        })
       })
+
       resolve(articles)
     })
   })
